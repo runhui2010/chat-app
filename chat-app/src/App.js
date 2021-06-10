79779cfbaf967a45ec74cb04467e6b559de7c75a
runhui2010 <52641048+runhui2010@users.noreply.github.com>
2021-06-09 23:55:40 -0700

    build user state

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -4,33 +4,44 @@ import {useEffect, useState,useContext} from 'react'
 import React from 'react'
 import Login from "./pages/Login";
 import Window from "./pages/Window";
+import GoogleLogin from 'react-google-login'
+import { useGoogleLogin } from 'react-use-googlelogin'
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
 import CurrUserContext from './components/CurrUserContext';
+import SignIn from './pages/SignIn';
 
 
 function App() {
-  const [currUser,setCurrUser]=useState({name:'',room:''})
+  const [currUser,setCurrUser]=useState({name:'',room:'',password:'',avatar:'',isSignedIn:false})
+
   
 
 
   return (
+    
     <Router>
       <Switch>
         <CurrUserContext.Provider value={[currUser,setCurrUser]}>
         <Route exact path='/'>
           <Login/>
-        </Route>
-        <Route path='/window'>
-          <Window/>
-        </Route>
-        </CurrUserContext.Provider>
-      </Switch>
-    </Router>
+      </Route>
+      <Route path='/signin'>
+        <SignIn/>
+      </Route>
+      <Route path='/window'>
+        <Window/>
+      </Route>
+      </CurrUserContext.Provider>
+    </Switch>
+  </Router>
+        
+    
      
   );
 }
